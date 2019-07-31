de0947bc9935e2723e2b3f5b630affb346df800e
SoulSeekkor <soulseekkor@live.com>
2019-07-31 18:44:44 -0500

    Updated to newer scripts.

--- bitwarden.sh
+++ bitwarden.sh
@@ -37,8 +37,8 @@ fi
 
 SCRIPTS_DIR="$OUTPUT/scripts"
 GITHUB_BASE_URL="https://raw.githubusercontent.com/SoulSeekkor/bitwarden-scripts/master"
-COREVERSION="1.30.4"
-WEBVERSION="2.10.1"
+COREVERSION="1.31.1"
+WEBVERSION="2.11.0"
 
 # Functions
 
